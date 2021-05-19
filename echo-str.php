589607004a188c7c0fe0e20cbd84a44c6e4c17f6
rishikumar1998 <84433404+rishikumar1998@users.noreply.github.com>
2021-05-19 11:23:01 +0530

    Add files via upload

---
+++ echo-str.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first page Php</title>
+</head>
+<body>
+<h1>my first page on php</h1>
+<?php
+    
+$txt="elephent"
+echo "Hello World!";
+echo "The Crow!";
+echo "Best Friend!";
+echo "The $txt!";
+
+
+?>
+<?php
+$txt = "Rishi";
+echo "My name is $txt!";
+?>
+    
+    <?php
+// This is a single-line comment
+$txt = "Rishi";
+echo "My name is $txt!";
+# This is also a single-line comment
+?>
+</body>
+</html>
